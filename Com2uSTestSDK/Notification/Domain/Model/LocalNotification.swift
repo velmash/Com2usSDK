5d26f0b9c2609ff7a84574db1939e898d6f3e12b
윤형찬 <72960152+velmash@users.noreply.github.com>
2025-03-27 16:07:52 +0900

    no message

---
+++ Com2uSTestSDK/Notification/Domain/Model/LocalNotification.swift
@@ -0,0 +1,15 @@
+//
+//   권한 확인.swift
+//  Com2uSTestSDK
+//
+//  Created by 윤형찬 on 3/27/25.
+//
+
+import Foundation
+
+struct LocalNotification: Identifiable {
+    let id: String
+    let title: String
+    let body: String
+    let triggerDate: Date
+}

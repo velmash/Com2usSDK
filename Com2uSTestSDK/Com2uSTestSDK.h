48cf1d9c20121745ccec9b9726da1d69d22139ff
윤형찬 <72960152+velmash@users.noreply.github.com>
2025-03-27 15:16:50 +0900

    Initial Commit

---
+++ Com2uSTestSDK/Com2uSTestSDK.h
@@ -0,0 +1,18 @@
+//
+//  Com2uSTestSDK.h
+//  Com2uSTestSDK
+//
+//  Created by 윤형찬 on 3/27/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Com2uSTestSDK.
+FOUNDATION_EXPORT double Com2uSTestSDKVersionNumber;
+
+//! Project version string for Com2uSTestSDK.
+FOUNDATION_EXPORT const unsigned char Com2uSTestSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Com2uSTestSDK/PublicHeader.h>
+
+
